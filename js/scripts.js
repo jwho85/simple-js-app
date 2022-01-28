97c121b1b61a81a0a5f88d7fc560960ac7435c45
jwho85 <98196552+jwho85@users.noreply.github.com>
2022-01-28 14:02:22 -0800

    Added 5 pokemon to pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world');
-
-var favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: "Bulbasaur", height: 0.7 , type:['grass','poison']},
+  {name: "Charmander", height: 0.6, type: ['fire']},
+  {name: "Squirtle", height: 0.5, type: ['water']},
+  {name: "Caterpie", height: 0.3, type: ['bug']},
+  {name: "Weedle", height: 0.3, type: ['bug','poison']}
+];
