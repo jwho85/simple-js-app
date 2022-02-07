3d873398fff0beb315279e186bac27c5f2deb09b
jwho85 <98196552+jwho85@users.noreply.github.com>
2022-02-07 11:16:00 -0800

    Created IIFE with getAll() and add() and redid forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
-//list of pokemon
-let pokemonList = [
-  {name: "Bulbasaur", height: 0.7 , type:['grass','poison']},
-  {name: "Charmander", height: 0.6, type: ['fire']},
-  {name: "Squirtle", height: 0.5, type: ['water']},
-  {name: "Caterpie", height: 0.3, type: ['bug']},
-  {name: "Weedle", height: 0.3, type: ['bug','poison']}
-];
+//list of pokemon (IIFE)
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: "Bulbasaur", height: 0.7 , type:['grass','poison']},
+    {name: "Charmander", height: 0.6, type: ['fire']},
+    {name: "Squirtle", height: 0.5, type: ['water']},
+    {name: "Caterpie", height: 0.3, type: ['bug']},
+    {name: "Weedle", height: 0.3, type: ['bug','poison']}
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
 // for (let i=0; i < pokemonList.length; i++){ //loops through pokemonList
 //   if (pokemonList[i].height > 0.6) { //finds tallest pokemon
@@ -15,7 +30,7 @@ let pokemonList = [
 //   }
 // }
 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.height > 0.6) { //finds tallest pokemon
     document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")" + " - Wow, that's big!" + "</p>");
   } else { //outputs other pokemon
