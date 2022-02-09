a8c0afe898591b1efc67f50a8d050de44adc3d32
jwho85 <98196552+jwho85@users.noreply.github.com>
2022-02-09 14:08:12 -0800

    Created showDetails(), added click function to button, updated button styles, updated page title

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//list of pokemon (IIFE)
+//IIFE
 let pokemonRepository = (function () {
   let pokemonList = [
     {name: "Bulbasaur", height: 0.7 , type:['grass','poison']},
@@ -15,10 +15,29 @@ let pokemonRepository = (function () {
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
+  
+  function addListItem(pokemon) {
+    let pokemonUl = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('custom-button');
+    listItem.appendChild(button);
+    pokemonUl.appendChild(listItem);
+    button.addEventListener('click', function (event) {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
@@ -31,9 +50,20 @@ let pokemonRepository = (function () {
 // }
 
 pokemonRepository.getAll().forEach(function(pokemon) {
-  if (pokemon.height > 0.6) { //finds tallest pokemon
-    document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")" + " - Wow, that's big!" + "</p>");
-  } else { //outputs other pokemon
-    document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")" + "</p>");
-  }
+
+  pokemonRepository.addListItem(pokemon);
+
+  // let pokemonUl = document.querySelector('.pokemon-list');
+  // let listItem = document.createElement('li');
+  // let button = document.createElement('button');
+  // button.innerText = pokemon.name;
+  // button.classList.add('custom-button');
+  // listItem.appendChild(button);
+  // pokemonUl.appendChild(listItem);
+
+  // if (pokemon.height > 0.6) { //finds tallest pokemon
+  //   document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")" + " - Wow, that's big!" + "</p>");
+  // } else { //outputs other pokemon
+  //   document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")" + "</p>");
+  // }
 });
